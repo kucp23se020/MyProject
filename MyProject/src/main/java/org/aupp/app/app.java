10aa77830142d41d319846232c4cb6cd38383f3c
winOS <kucp23se020@kzu.ac.in>
2023-09-15 19:20:48 +0700

    First Commit

---
+++ MyProject/src/main/java/org/aupp/app/app.java
@@ -0,0 +1,10 @@
+package org.aupp.app;
+
+import org.aupp.model.Employee; 
+public class app {
+	 public static void main(String[] args) { 
+		  Employee emp = new Employee(1,"Peter Parker"); 
+		  System.out.println(emp.getId() + " --- " + emp.getName()); 
+		 
+		 } 
+}
